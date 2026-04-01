a7f9040c297d1ceb051bc9f57ece8583c4e8adbb
Your Name <you@example.com>
2026-04-01 03:48:01 +0000

    Adapt repository layout and template files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setup_translate
+
+pkg = 'SystemPlugins.GigaBlueBluetoothSetup'
+setup(name='enigma2-plugin-systemplugins-bluetoothsetup',
+       version='3.0',
+       description='GigaBlue bluetooth plugin',
+       package_dir={pkg: 'GigaBlueBluetoothSetup'},
+       packages=[pkg],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo', 'plugin.png', 'bt_audio.png', 'bt_keyboard.png', 'bt_misc.png', 'bt_rc.png', 'keymap.xml', 'ble_local_keys', '_gbbt.so']},
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )
